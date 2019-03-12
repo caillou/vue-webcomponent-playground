40d87befc8fcfe26917e2105debb0e8b2fa303aa
Pierre Spring <pierre.spring@caillou.ch>
2019-03-12 16:53:31 +0100

    add new sbb-autocomplete component

--- src/components/TimetableSearch.vue
+++ src/components/TimetableSearch.vue
@@ -3,16 +3,37 @@
     <sbb-timetable-search
       @sbb-timetable-search_change="onLocationChange">
     </sbb-timetable-search>
+    <sbb-timetable-search>
+      <sbb-autocomplete
+        slot="origin"
+        name="origin"
+        :suggestions="originSuggestions"
+      ></sbb-autocomplete>
+      <sbb-autocomplete
+        slot="destination"
+        name="destination"
+        :suggestions="destinationSuggestions"
+      ></sbb-autocomplete>
+    </sbb-timetable-search>
   </div>
 </template>
 
 <script>
 import axios from 'axios'
+
+import defaultLocations from '../mock-data/default-locations'
 const CancelToken = axios.CancelToken
+const defaultLocationsJSON = JSON.stringify(defaultLocations)
 
 export default {
-
+  data () {
+    return {
+      destinationSuggestions: defaultLocationsJSON,
+      originSuggestions: defaultLocationsJSON
+    }
+  },
   methods: {
+
     onLocationChange (e) {
       const { type, value } = e.detail
       const cancelTokenKey = `${type}CancelToken`
---
+++ src/mock-data/default-locations.js
@@ -0,0 +1,22 @@
+export default [
+  {
+    label: 'Bern',
+    id: 8507000
+  },
+  {
+    label: 'Zürich HB',
+    id: 8503000
+  },
+  {
+    label: 'Fribourg',
+    id: 178
+  },
+  {
+    label: 'Basel',
+    id: 22
+  },
+  {
+    label: 'Genève',
+    id: 8501008
+  }
+]
